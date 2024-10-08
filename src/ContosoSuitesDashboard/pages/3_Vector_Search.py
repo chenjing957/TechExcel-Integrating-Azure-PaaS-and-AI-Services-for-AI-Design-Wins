f362dcf4ed600303b294c6c430651f778a248f6e
chenjing957 <107201795+chenjing957@users.noreply.github.com>
2024-10-08 15:25:42 +0000

    dash change

--- src/ContosoSuitesDashboard/pages/3_Vector_Search.py
+++ src/ContosoSuitesDashboard/pages/3_Vector_Search.py
@@ -55,7 +55,7 @@ def main():
                 st.write("## Results")
 
                 # Exercise 3 Task 3 TODO #6: Display the results as a table.
-                
+                st.write(vector_search_results)
                 #st.table(vector_search_results.json())
             else:
                 st.warning("Please enter a query.")
